7a1c3cfd77bfd1d429d1170bd3b0f3ace5e1219c
Joshua <Todaysjosh@gmail.com>
2022-03-31 20:46:54 +0200

    Changed language to English

--- src/app/components/adresse-bok/adresse-bok.component.ts
+++ src/app/components/adresse-bok/adresse-bok.component.ts
@@ -17,36 +17,8 @@ export class AdresseBokComponent implements OnInit {
     name: 'Rose',
     telephone: '12333329',
     email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
-  },{
-    name: 'Rose',
-    telephone: '12333329',
-    email: 'rose@titanic.us'
   }];
-
+  
   contactForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required]),

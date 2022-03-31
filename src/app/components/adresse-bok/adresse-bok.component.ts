5b05cbd8060e38c81ef8623fa2ff7b0268a0d9c5
Joshua <Todaysjosh@gmail.com>
2022-03-31 18:47:32 +0200

    added validation on form, styled adress-book component and fixed minor bugs

--- src/app/components/adresse-bok/adresse-bok.component.ts
+++ src/app/components/adresse-bok/adresse-bok.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ContactInformation } from 'src/app/interfaces/ContaxtInformation.interface';
 
 @Component({
@@ -10,30 +10,36 @@ import { ContactInformation } from 'src/app/interfaces/ContaxtInformation.interf
 export class AdresseBokComponent implements OnInit {
   
   contacts: ContactInformation[] = [{
-    name: 'temp',
-    telephone: 'temp',
-    email: 'temp'
+    name: 'Jack',
+    telephone: '92332231',
+    email: 'jack@titanic.us'
   },{
-    name: 'temp',
-    telephone: 'temp',
-    email: 'temp'
+    name: 'Rose',
+    telephone: '12333329',
+    email: 'rose@titanic.us'
   }];
 
   contactForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    telephone: new FormControl('')
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    telephone: new FormControl('', [Validators.required])
   });
 
   editingIndex: number | null = null;
-  
+  editButton: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
   submitContactForm(): void {
-    const contactInfo: ContactInformation = {
+    let contactInfo: ContactInformation;
+    if(!this.isFormFieldsValid()) {
+      return;
+    }
+
+    contactInfo = {
       name: this.contactForm.get('name')?.value,
       email: this.contactForm.get('email')?.value,
       telephone: this.contactForm.get('telephone')?.value
@@ -41,7 +47,7 @@ export class AdresseBokComponent implements OnInit {
 
     if(this.editingIndex !== null) {
       this.contacts[this.editingIndex].name = contactInfo.name;
-      this.contacts[this.editingIndex].telephone = contactInfo.telephone
+      this.contacts[this.editingIndex].telephone = contactInfo.telephone;
       this.contacts[this.editingIndex].email = contactInfo.email;
       this.editingIndex = null;
     } else {
@@ -49,11 +55,13 @@ export class AdresseBokComponent implements OnInit {
     }
 
     this.contactForm.reset();
+    console.log(this.contactForm.get('name')?.errors)
+    this.editButton = false;
   }
 
   onEditButtonClicked(index: number): void {
     this.editingIndex = index;
-    
+    this.editButton = true;
     const contact: ContactInformation = this.contacts[this.editingIndex];
     this.contactForm.get('name')?.setValue(contact.name);
     this.contactForm.get('telephone')?.setValue(contact.telephone);
@@ -64,4 +72,20 @@ export class AdresseBokComponent implements OnInit {
       this.contacts.splice(index, 1);
   }
 
+  isFormFieldsValid(): boolean {
+    if(this.contactForm.get('name')?.hasError('required')) {
+      return false;
+    }
+
+    if(this.contactForm.get('email')?.hasError('required') || 
+      this.contactForm.get('email')?.hasError('pattern')) {
+      return false;
+    }
+
+    if(this.contactForm.get('telephone')?.hasError('required')) {
+      return false;
+    }
+
+    return true;
+  }
 }
